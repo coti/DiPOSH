b11aa91b5888e3bf4b14a5c423d0d884397c48ff
Camille Coti <camille.coti@lipn.univ-paris13.fr>
2020-02-13 14:02:22 -0800

    Maybe this is needed

---
+++ config.h
@@ -0,0 +1,99 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the <boost/bind.hpp> header file. */
+#define HAVE_BOOST_BIND_HPP 1
+
+/* Define to 1 if you have the <boost/interprocess/managed_shared_memory.hpp>
+   header file. */
+#define HAVE_BOOST_INTERPROCESS_MANAGED_SHARED_MEMORY_HPP 1
+
+/* Define to 1 if you have the <boost/system/error_code.hpp> header file. */
+#define HAVE_BOOST_SYSTEM_ERROR_CODE_HPP 1
+
+/* Define to 1 if you have the <boost/thread.hpp> header file. */
+#define HAVE_BOOST_THREAD_HPP 1
+
+/* define if the compiler supports basic C++11 syntax */
+/* #undef HAVE_CXX11 */
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the `boost_thread' library (-lboost_thread). */
+#define HAVE_LIBBOOST_THREAD 1
+
+/* Define to 1 if you have the `boost_thread-mt' library (-lboost_thread-mt).
+   */
+/* #undef HAVE_LIBBOOST_THREAD_MT */
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define if you have POSIX threads libraries and header files. */
+#define HAVE_PTHREAD 1
+
+/* Have PTHREAD_PRIO_INHERIT. */
+#define HAVE_PTHREAD_PRIO_INHERIT 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to the sub-directory where libtool stores uninstalled libraries. */
+#define LT_OBJDIR ".libs/"
+
+/* Name of package */
+#define PACKAGE "posh--paris-openshmem"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "camille.coti@lipn.univ-paris13.fr"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "POSH: Paris OpenSHMEM"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "POSH: Paris OpenSHMEM 0.2"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "posh--paris-openshmem"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.2"
+
+/* Define to necessary symbol if this constant uses a non-standard name on
+   your system. */
+/* #undef PTHREAD_CREATE_JOINABLE */
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "0.2"
+
+/* Define to 1 if `lex' declares `yytext' as a `char *' by default, not a
+   `char[]'. */
+/* #undef YYTEXT_POINTER */
